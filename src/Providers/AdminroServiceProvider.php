38077a8b51cf12b42a0ac6e831a77e806f540788
Abed Tawil <aboditawil@gmail.com>
2022-02-10 16:07:40 +0200

    add livewire classes

--- src/Providers/AdminroServiceProvider.php
+++ src/Providers/AdminroServiceProvider.php
@@ -4,7 +4,10 @@ namespace Adminro\Providers;
 
 use Adminro\Console\Commands\RefreshPermissions;
 use Adminro\Console\Commands\RefreshTablesWithDummy;
+use Adminro\Livewire\DynamicSelect;
+use Adminro\Livewire\ModelSelect;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AdminroServiceProvider extends ServiceProvider
 {
@@ -15,7 +18,12 @@ class AdminroServiceProvider extends ServiceProvider
   public function boot()
   {
     $this->loadViewsFrom(__DIR__ . '/../resources/views', 'adminro');
+
     $this->publishes([__DIR__ . '/../public' => public_path('vendor/adminro')], 'adminro::public');
+
+    Livewire::component('model-select', ModelSelect::class);
+    Livewire::component('dynamic-select', DynamicSelect::class);
+
     if ($this->app->runningInConsole()) {
       $this->commands([
         RefreshPermissions::class,
