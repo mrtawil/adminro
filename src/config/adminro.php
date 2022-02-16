10b9b0f2a5fe89b85d9f82a75cc3657b538ed00f
Abed Tawil <aboditawil@gmail.com>
2022-02-16 20:35:19 +0200

    add description to adminro config

--- src/config/adminro.php
+++ src/config/adminro.php
@@ -1,9 +1,30 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Forms
+    |--------------------------------------------------------------------------
+    |
+    | Assign default forms records
+    |
+    */
+
     'forms' => [
         'select' => [],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Model Owner Settings Manager
+    |--------------------------------------------------------------------------
+    |
+    | Two static methods should be included in the class assigned
+    | - removeOwnerSettings
+    | - storeOwnerSettings
+    |
+    */
 
     'model_owner_settings_manager' => null,
 ];
