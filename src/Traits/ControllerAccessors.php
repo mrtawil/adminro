16798f9c7ab2cbfe7d1176f4bddb2b6c694e5ec8
Abed Tawil <aboditawil@gmail.com>
2022-05-21 15:19:22 +0300

    add return types to controller accessors

--- src/Traits/ControllerAccessors.php
+++ src/Traits/ControllerAccessors.php
@@ -2,7 +2,19 @@
 
 namespace Adminro\Traits;
 
+use Adminro\Controllers\Actions;
+use Adminro\Controllers\Authentication;
 use Adminro\Controllers\ControllerSettings;
+use Adminro\Controllers\DataTable;
+use Adminro\Controllers\FormFields;
+use Adminro\Controllers\GlobalVariables;
+use Adminro\Controllers\Info;
+use Adminro\Controllers\Model;
+use Adminro\Controllers\Request;
+use Adminro\Controllers\Route;
+use Adminro\Controllers\Select;
+use Adminro\Controllers\Service;
+use Adminro\Controllers\Subheader;
 
 trait ControllerAccessors
 {
@@ -11,62 +23,62 @@ trait ControllerAccessors
         return $this->controllerSettings;
     }
 
-    public function auth()
+    public function auth(): Authentication
     {
         return $this->controllerSettings()->auth();
     }
 
-    public function info()
+    public function info(): Info
     {
         return $this->controllerSettings()->info();
     }
 
-    public function subheader()
+    public function subheader(): Subheader
     {
         return $this->controllerSettings()->subheader();
     }
 
-    public function actions()
+    public function actions(): Actions
     {
         return $this->controllerSettings()->actions();
     }
 
-    public function model()
+    public function model(): Model
     {
         return $this->controllerSettings()->model();
     }
 
-    public function formFields()
+    public function formFields(): FormFields
     {
         return $this->controllerSettings()->formFields();
     }
 
-    public function request()
+    public function request(): Request
     {
         return $this->controllerSettings()->request();
     }
 
-    public function route()
+    public function route(): Route
     {
         return $this->controllerSettings()->route();
     }
 
-    public function dataTable()
+    public function dataTable(): DataTable
     {
         return $this->controllerSettings()->dataTable();
     }
 
-    public function select()
+    public function select(): Select
     {
         return $this->controllerSettings()->select();
     }
 
-    public function services()
+    public function services(): Service
     {
         return $this->controllerSettings()->services();
     }
 
-    public function globalVariables()
+    public function globalVariables(): GlobalVariables
     {
         return $this->controllerSettings()->globalVariables();
     }
