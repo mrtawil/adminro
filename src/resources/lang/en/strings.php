337365fddef475012edeb66dc5bb9ab1546e96a3
Abed Tawil <aboditawil@gmail.com>
2022-05-21 13:51:47 +0300

    add status column to datatable with localization

---
+++ src/resources/lang/en/strings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'status_0' => 'Unpublished',
+    'status_1' => 'Published',
+    'status_2' => 'Under Review',
+    'status_3' => 'Abandoned',
+    'status_4' => 'Pending',
+    'status_5' => 'Deleted',
+];
---
+++ src/resources/views/includes/dashboard/datatables/columns/status.blade.php
@@ -0,0 +1,3 @@
+{{-- <div>{{ Lang::get('adminro::strings.status_1') }}</div> --}}
+<div>{{ trans('adminro::strings.status_1') }}</div>
+{{-- <div>@lang('adminro::strings.status_1')</div> --}}
