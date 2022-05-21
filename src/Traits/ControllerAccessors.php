586db89730c9aa2abc9e63e8cf98c1d261850403
Abed Tawil <aboditawil@gmail.com>
2022-05-21 15:15:50 +0300

    add the reset of the accessors

--- src/Traits/ControllerAccessors.php
+++ src/Traits/ControllerAccessors.php
@@ -11,14 +11,64 @@ trait ControllerAccessors
         return $this->controllerSettings;
     }
 
+    public function auth()
+    {
+        return $this->controllerSettings()->auth();
+    }
+
     public function info()
     {
         return $this->controllerSettings()->info();
     }
 
+    public function subheader()
+    {
+        return $this->controllerSettings()->subheader();
+    }
+
+    public function actions()
+    {
+        return $this->controllerSettings()->actions();
+    }
+
+    public function model()
+    {
+        return $this->controllerSettings()->model();
+    }
+
     public function formFields()
     {
         return $this->controllerSettings()->formFields();
+    }
+
+    public function request()
+    {
+        return $this->controllerSettings()->request();
+    }
+
+    public function route()
+    {
+        return $this->controllerSettings()->route();
+    }
+
+    public function dataTable()
+    {
+        return $this->controllerSettings()->dataTable();
+    }
+
+    public function select()
+    {
+        return $this->controllerSettings()->select();
+    }
+
+    public function services()
+    {
+        return $this->controllerSettings()->services();
+    }
+
+    public function globalVariables()
+    {
+        return $this->controllerSettings()->globalVariables();
     }
 
     public function item()
