70104f00f331b40a236f79d3bf2e25b9ae3edae0
Abed Tawil <aboditawil@gmail.com>
2022-06-04 14:55:08 +0300

    update status column to be dynamic

--- src/resources/views/includes/dashboard/datatables/columns/status.blade.php
+++ src/resources/views/includes/dashboard/datatables/columns/status.blade.php
@@ -1,3 +1,1 @@
-{{-- <div>{{ Lang::get('adminro::strings.status_1') }}</div> --}}
-<div>{{ trans('adminro::strings.status_1') }}</div>
-{{-- <div>@lang('adminro::strings.status_1')</div> --}}
+<div>{{ trans('adminro::strings.status_' . $item->status) }}</div>
